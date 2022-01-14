23bf896e4244a8ce864121e2fdc9a8c67a7a6c7f
Akash Bhagat <bhagatakash0123@gmail.com>
2022-01-14 10:46:59 +0530

    Created custom user model & changed some models

--- Blog/models.py
+++ Blog/models.py
@@ -1,21 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Article(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     category = models.CharField(max_length=50)
     thumbnail = models.ImageField(upload_to = 'Article/thumbnails/')
     timestamp = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200, null=False, blank= False)
     description = models.CharField(max_length=1000, null=False, blank=False)
+    likes = models.ManyToManyField(get_user_model(), related_name="post_like")
 
     def __str__(self) -> str:
         return self.title
 
 class ArticleComment(models.Model):
     Article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     comment_text = models.CharField(max_length=500, blank=False, null=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
