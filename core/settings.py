b2209cfaab361f30d92ebbee3906cdcc944b6b69
Akash3470399 <78096661+Akash3470399@users.noreply.github.com>
2022-03-03 07:35:40 +0530

    Revert "Master"

--- core/settings.py
+++ core/settings.py
@@ -83,24 +83,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    
- }
-
 # DATABASES = {
-#     'default':{
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':env('DATABASE_NAME'),
-#         'USER':env('DATABASE_USER'),
-#         'PASSWORD':env('DATABASE_PASSWORD'),
-#         'HOST':'localhost',
-#         'PORT':'5432'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':env('DATABASE_NAME'),
+        'USER':env('DATABASE_USER'),
+        'PASSWORD':env('DATABASE_PASSWORD'),
+        'HOST':'localhost',
+        'PORT':'5432'
+    }
+}
 
 # to use custom User model
 AUTH_USER_MODEL = 'User.CustomUser'
