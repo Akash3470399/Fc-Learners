8d7274fc32eb46baeb41cf52e1fbe618a351dac7
Akash Bhagat <bhagatakash0123@gmail.com>
2022-03-06 10:45:08 +0530

    Tested after git conflict

--- StudyMaterial/urls.py
+++ StudyMaterial/urls.py
@@ -5,6 +5,6 @@ app_name = 'StudyMaterial'
 urlpatterns = [
     path('', views.index, name = "study_home"),
     path('study-material-listing/',views.study_material_listing,name='study_material_listing'),
-    path('add_study/',views.add_study_material,name='add_study'),
+    path('add_study_material/',views.add_study_material,name='add_study'),
     path("search-notes/", views.search_notes, name = "search_notes"),
 ]
